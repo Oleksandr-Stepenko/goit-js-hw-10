0dd671b1bbe6dd9a43901f93d62dfa43f5e67106
Oleksandr-Stepenko <113541212+Oleksandr-Stepenko@users.noreply.github.com>
2023-03-16 19:12:19 +0200

    add hw 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener('input', debounce(inputNameCountry, DEBOUNCE_DELAY));
+
+function inputNameCountry(e) {
+  e.preventDefault();
+  const nameOfCountry = searchBox.value.trim();
+
+  if (nameOfCountry === '') {
+    countryInfo.innerHTML = '';
+    countriesList.innerHTML = '';
+    return;
+  }
+
+	fetchCountries(nameOfCountry)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        countryInfo.innerHTML = '';
+        countriesList.innerHTML = '';
+        return;
+      }
+
+      if (countrs.length <= 10) {
+        const listMarkup = countries.map(country => showCountries(country));
+        countriesList.innerHTML = listMarkup.join('');
+        countryInfo.innerHTML = '';
+      }
+
+      if (countries.length === 1) {
+        const markup = countries.map(country => showInfoCountry(country));
+        countryInfo.innerHTML = markup.join('');
+        countriesList.innerHTML = '';
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      countryInfo.innerHTML = '';
+      countriesList.innerHTML = '';
+      return error;
+    });
+}
+
+function showInfoCountry({ flags, name, capital, population, languages }) {
+  return `
+	<div>
+    <div>
+      <img src="${flags.svg}" alt="${name.official}" width="50" />
+      <h2 class="country-name">${name.official}</h2>
+    </div>
+    <p><span>Capital:</span> ${capital}</p>
+    <p><span>Population:</span> ${population}</p>
+    <p><span>Languages:</span> ${Object.values(languages)}</p>
+  </div>
+  `;
+}
+
+function showCountries({ flags, name }) {
+  return `
+  <li>
+    <img src="${flags.svg}" alt="${name.official}" width="25" />
+    <h2>${name.official}</h2>
+  </li>
+  `;
+}
